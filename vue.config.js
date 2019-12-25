1d414ed0ed2cb6143c6caf32a32a6dda01dc67cf
Your Name <you@example.com>
2019-12-25 16:58:39 +0800

    --

--- vue.config.js
+++ vue.config.js
@@ -11,12 +11,12 @@ module.exports = {
     // baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
     devServer: {
         port: 8080, // 端口号
-        host: '192.168.0.104',
+	host: '0.0.0.0',
         https: false, // https:{type:Boolean}
         open: true, //配置自动启动浏览器
         proxy: { // 配置跨域
             '/api': {
-                target: 'http://192.168.0.104:8088',
+                target: 'http://106.13.163.18:8088',
                 ws: true,
                 changOrigin: true,
                 pathRewrite: {

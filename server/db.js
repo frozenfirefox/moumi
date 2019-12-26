98de34450ac81517a925eda185006739ff813648
Administrator <admin@example.com>
2019-12-26 16:29:39 +0800

    添加

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@
 * @Author: Alpha
 * @Date:   2019-12-23 10:39:40
 * @Last Modified by:   Alpha
-* @Last Modified time: 2019-12-23 12:24:13
+* @Last Modified time: 2019-12-26 16:24:43
 */
 
 'use strict';
@@ -10,7 +10,7 @@
 // Schema、Model、Entity或者Documents的关系请牢记，Schema生成Model，Model创造Entity，Model和Entity都可对数据库操作造成影响，但Model比Entity更具操作性。
 const mongoose = require('mongoose');
 // 连接数据库 如果不自己创建 默认test数据库会自动生成
-mongoose.connect('mongodb://test:test@127.0.0.1:27017/test'); // 地址跟第一步的地址对应。//直接连接数据库
+mongoose.connect('mongodb://127.0.0.1:27017/face'); // 地址跟第一步的地址对应。//直接连接数据库
 
 // 为这次连接绑定事件
 const db = mongoose.connection;
